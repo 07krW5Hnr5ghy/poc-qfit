6d996941575f4100cb90916d89e8ecc5f770ce2d
Juan Pablo Romero <jryn145@gmail.com>
2022-12-13 16:05:43 -0500

    categories design

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import Slider from '../components/Slider';
-import React from 'react'
 import Announcement from '../components/Announcement';
 import Navbar from '../components/Navbar';
+import Categories from '../components/Categories';
 
 const Home = () => {
   return (
@@ -9,6 +9,7 @@ const Home = () => {
       <Announcement/>
       <Navbar/>
       <Slider/>
+      <Categories/>
     </div>
   )
 }
