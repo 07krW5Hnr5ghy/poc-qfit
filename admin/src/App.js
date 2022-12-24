8421bdb02074c39f0a0f1c6c039ee71744b05ad3
Juan Pablo Romero <jryn145@gmail.com>
2022-12-23 19:01:45 -0500

    react material ui table

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,17 +1,27 @@
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import Home from "./pages/home/Home";
+import UserList from "./pages/userList/UserList";
 import "./app.css";
+import { 
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Topbar/>
-      <div className="container">
-        <Sidebar/>
-        <Home/>
+      <div>
+        <Topbar/>
+        <div className="container">
+          <Sidebar/>
+          <Routes>
+            <Route exact path="/" element={<Home/>}/>
+            <Route path="/users" element={<UserList/>}/>
+          </Routes>
+        </div>
       </div>
-    </div>
   );
 }
 
