20d387603f7f42942a0a6570154c4367fd5ac175
Juan Pablo Romero <jryn145@gmail.com>
2022-12-24 14:48:41 -0500

    product list data grid

--- admin/src/App.js
+++ admin/src/App.js
@@ -11,6 +11,7 @@ import {
 } from "react-router-dom";
 import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
+import ProductList from "./pages/productList/ProductList";
 
 function App() {
   return (
@@ -23,6 +24,8 @@ function App() {
             <Route path="/users" element={<UserList/>}/>
             <Route path="/user/:userId" element={<User/>}/>
             <Route path="/newUser" element={<NewUser/>}/>
+            <Route path="/products" element={<ProductList/>}/>
+            <Route path="/product/:productId" element={<NewUser/>}/>
           </Routes>
         </div>
       </div>
